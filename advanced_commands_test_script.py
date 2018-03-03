87eedb50acc3c6d4ab54e8fca7cfc45aeb918c61
basilwong <wong.bas@gmail.com>
2018-03-03 13:44:36 -0800

    Adding another file with the commands, to be referenced when testing and controlling the drone.

--- advanced_commands_test_script.py
+++ advanced_commands_test_script.py
@@ -16,7 +16,7 @@ vehicle = dronekit.connect(connection_string, wait_ready=True)
 # Arms the vehicle and takes of the specified altitude.
 arm_and_takeoff(altitude, vehicle)
 
-# Section for entering commands for test run. 
+# Section for entering commands for test run.
 # ----------------------------------------------------------------------------------------------------------------------
 
 
